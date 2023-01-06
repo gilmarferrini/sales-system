c4468295c11802e4e3d52f56041c43fbd08a0cf2
gilmarferrini <ferrinigilmar@gmail.com>
2023-01-06 17:13:15 -0300

    test: should return false if cpf is invalid

--- test/main.spec.ts
+++ test/main.spec.ts
@@ -9,7 +9,29 @@ describe('validate', () => {
     '062.819.790-01'
   ]
 
+  const invalidCpfs = [
+    '111.111.111-11',
+    '222.222.222-22',
+    '333.333.333-33',
+    '444.444.444-44',
+    '555.555.555-55',
+    '666.666.666-66',
+    '777.777.777-77',
+    '888.888.888.88',
+    '999.999.999.999',
+    '981.654.321-20',
+    '561.803.980-37',
+    '071.159.720-40',
+    '061.819.790-01',
+    null,
+    undefined
+  ]
+
   it.each(validCpfs)('Should return true if cpf is valid: %s', (cpf) => {
     expect(validate(cpf)).toBeTruthy()
+  })
+
+  it.each(invalidCpfs)('Should return false if cpf is invalid: %s', (cpf) => {
+    expect(validate(cpf)).toBeFalsy()
   })
 })
