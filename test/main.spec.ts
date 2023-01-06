6b1ca3a78013f389ff0b66b54139d7ae25dcddc1
gilmarferrini <ferrinigilmar@gmail.com>
2023-01-06 17:08:38 -0300

    refactor: change test to use validCpfs array

--- test/main.spec.ts
+++ test/main.spec.ts
@@ -1,8 +1,15 @@
 import { validate } from "../src/main"
 
 describe('validate', () => {
-  it('Should return true if cpf is valid', () => {
-    const isValid = validate('987.654.321-00')
-    expect(isValid).toBeTruthy()
+
+  const validCpfs = [
+    '987.654.321-00',
+    '566.803.980-37',
+    '077.159.720-70',
+    '062.819.790-01'
+  ]
+
+  it.each(validCpfs)('Should return true if cpf is valid', (cpf) => {
+    expect(validate(cpf)).toBeTruthy()
   })
 })
